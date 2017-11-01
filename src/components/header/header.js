d2d6917778e672a501f6148c05360e2c85716e43
ZhengZhouXu <a819384406@163.com>
2017-11-01 18:17:19 +0800

    清理文件，添加字体文件

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,29 @@ import CSSModules from 'react-css-modules'
 import styles from './header.css'
 
 class Header extends Component {
+	
+	constructor (props) {
+		super(props)
+		this.state = {background: "rgb(94, 117, 228)", color: '#fff'}
+	}
+
+	getHeaderHeight () {
+		return window.innerHeight * 0.09
+	}
+
 	render () {
+		const height = this.getHeaderHeight() + 'px'
+		const style = {
+			height: height,
+			lineHeight: height,
+			background: this.state.background,
+			color: this.state.color
+		}
 		return (
-			<header styleName="header">
+			<header styleName="header" style={style}>
 				<span styleName="left">1</span>
 				<span styleName="text">2</span>
-				<span styleName="rigth">3</span>
+				<span styleName="right">3</span>
 			</header>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header/header';
 import registerServiceWorker from './registerServiceWorker';
+import './common/styles/reset.css'
+import './common/styles/base.css'
 
 ReactDOM.render(<Header />, document.getElementById('root'));
 registerServiceWorker();
